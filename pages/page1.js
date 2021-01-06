1c179fbec1c394e91f279305cbbb60aca19cd487
jeo19 <terrakkk@outlook.com>
2021-01-06 09:51:59 +0800

    add:The header for seo step1

--- pages/page1.js
+++ pages/page1.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
 export default function Page1(){
     return (
         <div>
+        <Head>
+        <title>page1</title>
+      </Head>
+       <Head>
+        <meta name="description" content="hello world" />
+      </Head>
+        <div>
             <p>This is Homepage</p>
+        </div>
         </div>
     )
 }
