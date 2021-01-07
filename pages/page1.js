375d64cc86a0234268f7734bd67318d3d3d3ce9c
jeo19 <terrakkk@outlook.com>
2021-01-07 09:04:28 +0800

    add:The header for seo step2

--- pages/page1.js
+++ pages/page1.js
@@ -2,15 +2,22 @@ import Head from "next/head";
 export default function Page1(){
     return (
         <div>
-        <Head>
-        <title>page1</title>
-      </Head>
-       <Head>
-        <meta name="description" content="hello world" />
-      </Head>
-        <div>
-            <p>This is Homepage</p>
+          <p>This is home page</p>
+          <img src="/static/kangaroo-c.png" />
+          <Head>
+            <title>page1</title>
+          </Head>
+          <Head>
+            <meta name="description" content="hello world" />
+          </Head>
+          <style jsx>
+            {`
+              p {
+                color: blue;
+                font-size: 18pt;
+              }
+            `}
+          </style>
         </div>
-        </div>
-    )
+      );
 }
